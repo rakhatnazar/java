ee92835bdd305a5ae630ef805e4275b8d90e81ca
rakhatnazar <rnazar075@gmail.com>
2018-11-21 12:28:21 -0600

    JavaProject

---
+++ Packages/src/com/cybertek/package2/Class2.java
@@ -0,0 +1,22 @@
+package com.cybertek.package2;
+
+//import com.cybertek.package1.*;
+//import com.cybertek.package1.Class1;
+
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		
+		//there are 3 ways to access package from outside the package
+		//1-import package.*
+		//2-import package.className
+		//3-fully quialified name
+		
+		com.cybertek.package1.Class1 cl=new com.cybertek.package1.Class1();
+		
+		
+
+	}
+
+}
